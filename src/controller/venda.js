d91e009407d82d1ea923847a15da03afd3253a61
KalebeLopes <calbkalebe@gmail.com>
2021-05-05 18:45:30 -0300

    feat: implements venda by date

--- src/controller/venda.js
+++ src/controller/venda.js
@@ -12,14 +12,50 @@ class VendaController {
     }
   }
 
+  async getByDate(req, res) {
+    const {startDate, endDate} = req.query
+    // console.log(new Date(startDate).toDateString(), ' ', new Date(endDate).toDateString())
+    try {
+      const vendas = await this.Venda.find({
+        "data": {
+          $eq: new Date(startDate).toDateString()
+          // $gte: new Date(startDate).toDateString(),
+          // $lte: new Date(endDate).toDateString()
+        }
+      })
+      console.log(vendas)
+      res.send(vendas)
+    } catch (err) {
+      res.status(404).send(err.message);
+    }
+  }
+
   async cadastrar(req, res) {
-    const newVenda = new this.Venda(req.body)
+    const vendas = req.body.vendas
+    let data = new Date(req.body.data).toDateString()
+
+    const newVenda = new this.Venda(
+      {
+        vendas: vendas,
+        data: data 
+      }
+    )
     // console.log(req.body.email)
     try {
       await newVenda.save();
       return res.status(201).send(newVenda);
     } catch (err) {
       return res.status(422).send(err.message);
+    }
+  }
+
+  async excluirAll(req, res) {
+    try {
+      this.Venda.deleteMany({}, () => {
+        res.send('todas as vendas excluidas')
+      })
+    } catch (err) {
+      res.status(400).send(err.message)
     }
   }
 
