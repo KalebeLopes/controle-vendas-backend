95ea1551769d4d961c49108d54098a52dd10582d
KalebeLopes <calbkalebe@gmail.com>
2021-05-05 18:46:12 -0300

    refactor: update routers

--- src/routers/venda.js
+++ src/routers/venda.js
@@ -5,12 +5,22 @@ const VendaModel = require('../models/venda')
 
 const vendaController = new VendaController(VendaModel)
 
+// rotas sempre no singular
+
 router.get('/', (req, res) => {
   vendaController.get(req, res)
 })
 
+router.get('/date_range', (req, res) => {
+  vendaController.getByDate(req, res)
+})
+
 router.post('/', (req, res) => {
   vendaController.cadastrar(req, res)
+})
+
+router.delete('/', (req, res) => {
+  vendaController.excluirAll(req, res)
 })
 
 module.exports = router
