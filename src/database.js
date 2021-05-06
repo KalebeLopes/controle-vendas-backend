6c1c05c95ab558178b41379e23da7e4071bc2248
KalebeLopes <calbkalebe@gmail.com>
2021-05-06 12:12:54 -0300

    chore: connection with localhost database

--- src/database.js
+++ src/database.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const mongoose = require('mongoose')
 const mongoUrl = process.env.URL
+const mongoLocalHost = 'mongodb://localhost/controleVendas'
 
 const connect = () => {
-  mongoose.connect(mongoUrl, {
+  mongoose.connect(mongoLocalHost, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true
   })
 }
 
